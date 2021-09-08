fa93d3c1286929540523fbafa6d9a08ccbe84a0e
1005200 <mdspiker123@gmail.com>
2021-09-08 18:39:26 +0700

    biki api cient get transaksi

--- front/application/models/M_laporan.php
+++ front/application/models/M_laporan.php
@@ -1,120 +1,102 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+use GuzzleHttp\Client;
+
 class M_laporan extends CI_Model
 {
 
-    protected $laporan = 'laporan';
-    protected $produk = 'produk';
-    protected $cetak_laporan = 'cetak_laporan';
+    private $_client;
 
-    // join 
-    protected $Jproduk;
-    protected $Jcetak;
-
-    //select 
-    protected $Sproduk;
-
-    // order by
-    protected $Blaporan;
-    protected $Bproduk;
-    protected $Bcetak;
-
-    // construct
     public function __construct()
     {
         parent::__construct();
-
-        // select
-        $this->Sproduk = "$this->laporan.id, $this->laporan.id_produk, $this->produk.nama, $this->produk.harga_jual, $this->produk.harga_modal, $this->produk.gambar, $this->laporan.jumlah, $this->laporan.total_jual, $this->laporan.total_modal, $this->laporan.laba, $this->laporan.date_created, $this->laporan.date_modify,";
-
-        //join
-        $this->Jproduk = "$this->produk.id = $this->laporan.id_produk";
-        $this->Jcetak = "$this->produk.id = $this->cetak_laporan.id_produk";
-
-        // ordey by
-        $this->Blaporan = "$this->laporan.id";
-        $this->Bproduk = "$this->produk.id";
-        $this->Bcetak = "$this->cetak_laporan.id";
+        $this->_client = new Client([
+            'base_uri' => 'http://localhost/laporan-keuangan/back/',
+//            'auth' => ['admin', '1234']
+        ]);
     }
 
     // get laporan
     public function getLaporan($id = null)
     {
-        if ($id != null) {
-            $this->db->where("$this->laporan.id", $id);
-        }
-        $this->db->select($this->Sproduk);
-        $this->db->join($this->produk, $this->Jproduk);
-        $this->db->order_by($this->Blaporan, 'DESC');
-        return $this->db->get($this->laporan)->result_array();
+		$response = $this->_client->request('GET', 'transaksi', [
+//			'query' => [
+////				'X-API-KEY' => 'made123'
+//			]
+		]);
+
+		$result = json_decode($response->getBody()->getContents(), true);
+
+		return $result['result'];
     }
+
 
     // add laporan
     public function addLaporan($data)
     {
 
-        $this->db->insert($this->laporan, $data);
+//        $this->db->insert($this->laporan, $data);
     }
 
     // hapus laporan
     public function hapusLaporan($id)
     {
 
-        $this->db->delete($this->laporan, ['id' => $id]);
+//        $this->db->delete($this->laporan, ['id' => $id]);
     }
 
     // edit laporan
     public function editLaporan($data, $id)
     {
 
-        $this->db->update($this->laporan, $data, ['id' => $id]);
+//        $this->db->update($this->laporan, $data, ['id' => $id]);
     }
 
     // get produk
     public function getProduk($id = null)
     {
-        if ($id != null) {
-            $this->db->where(['id' => $id]);
-        }
-
-        $this->db->order_by($this->Bproduk, 'desc');
-
-        return $this->db->get($this->produk)->result_array();
+//        if ($id != null) {
+//            $this->db->where(['id' => $id]);
+//        }
+//
+////        $this->db->order_by($this->Bproduk, 'desc');
+//
+//        return $this->db->get($this->produk)->result_array();
     }
 
     // add produk
     public function addProduk($data)
     {
-        $this->db->insert($this->produk, $data);
+//        $this->db->insert($this->produk, $data);
     }
 
     // hapus produk
     public function hapusProduk($id)
     {
-        $this->db->delete($this->produk, ['id' => $id]);
+//        $this->db->delete($this->produk, ['id' => $id]);
     }
 
     // edit produk
     public function editProduk($id, $data)
     {
-        $this->db->update($this->produk, $data, ['id' => $id]);
+//        $this->db->update($this->produk, $data, ['id' => $id]);
     }
 
     // cetak laporan 
     public function cetakLaporan($data)
     {
 
-        $this->db->insert($this->cetak_laporan, $data);
+//        $this->db->insert($this->cetak_laporan, $data);
     }
 
     // get cetak
     public function getCetak()
     {
         
-        $this->db->order_by($this->Bcetak, 'desc');
+//        $this->db->order_by($this->Bcetak, 'desc');
         
-        $this->db->join($this->produk, $this->Jcetak);
-        return $this->db->get($this->cetak_laporan)->result_array();
+//        $this->db->join($this->produk, $this->Jcetak);
+//        return $this->db->get($this->cetak_laporan)->result_array();
     }
 }
