14a710ddecf5c31243afd14d7deefdf357085f24
1005200 <mdspiker123@gmail.com>
2021-09-09 00:11:43 +0700

    botom navigation menu with fragment

--- android/app/src/main/java/com/made_suande_1811010036/android/MainActivity.java
+++ android/app/src/main/java/com/made_suande_1811010036/android/MainActivity.java
@@ -1,8 +1,14 @@
 package com.made_suande_1811010036.android;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.made_suande_1811010036.android.fragment.HomeFragment;
+import com.made_suande_1811010036.android.fragment.ProdukFragment;
+import com.made_suande_1811010036.android.fragment.TransaksiFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loadFragment(new HomeFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomMenu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment fragment = null;
+            switch (item.getItemId()){
+                case R.id.home_menu:
+                    fragment = new HomeFragment();
+                    break;
+                case R.id.produk_menu:
+                    fragment = new ProdukFragment();
+                    break;
+                case  R.id.transaksi_menu:
+                    fragment = new TransaksiFragment();
+                    break;
+            }
+            return loadFragment(fragment);
+
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment)
+    {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fl_container, fragment)
+                    .commit();
+            return  true;
+        }
+        return false;
     }
 }
