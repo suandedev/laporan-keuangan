87b58fa88951d7b8236c12c80c5a9576f7d6d619
1005200 <mdspiker123@gmail.com>
2021-09-13 10:46:58 +0700

    retrievve data from my produk

--- android/app/src/main/java/com/made_suande_1811010036/android/fragment/ProdukFragment.java
+++ android/app/src/main/java/com/made_suande_1811010036/android/fragment/ProdukFragment.java
@@ -3,64 +3,72 @@ package com.made_suande_1811010036.android.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.made_suande_1811010036.android.R;
+import com.made_suande_1811010036.android.adapter.ProdukAdapter;
+import com.made_suande_1811010036.android.model.GetProduk;
+import com.made_suande_1811010036.android.model.Produk;
+import com.made_suande_1811010036.android.rest.ApiClient;
+import com.made_suande_1811010036.android.rest.ApiInterface;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProdukFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ProdukFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public ProdukFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProdukFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProdukFragment newInstance(String param1, String param2) {
-        ProdukFragment fragment = new ProdukFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    ApiInterface mApiInterface;
+    private RecyclerView mReciclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    public static ProdukFragment pf;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_produk, container, false);
+        View view = inflater.inflate(R.layout.fragment_produk, container, false);
+        dataInit(view);
+    return view;
+    }
+
+    private void dataInit(View view) {
+        mReciclerView = view.findViewById(R.id.recyclerView_produk);
+        mLayoutManager = new LinearLayoutManager(getContext());
+        mReciclerView.setLayoutManager(mLayoutManager);
+        mApiInterface = ApiClient.getClient().create(ApiInterface.class);
+        pf = this;
+        refresh();
+    }
+
+    private void refresh() {
+        Call<GetProduk> produkCall = mApiInterface.getProduk();
+        produkCall.enqueue(new Callback<GetProduk>() {
+            @Override
+            public void onResponse(Call<GetProduk> call, Response<GetProduk> response) {
+                List<Produk> produkList = response.body().getListProduk();
+                Log.d("retrofit get", "jumlah data kontak" +
+                        String.valueOf(produkList.size()));
+                mAdapter = new ProdukAdapter(produkList);
+                mReciclerView.setAdapter(mAdapter);
+            }
+
+            @Override
+            public void onFailure(Call<GetProduk> call, Throwable t) {
+                Log.d("retrofit get", t.toString());
+            }
+        });
     }
 }
